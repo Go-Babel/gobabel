0e592973cc8ea9733a8365bafca80c894c117ecc
Igor <igor_midev@outlook.com>
2025-08-05 13:26:50 -0300

    Enhances localization key replacement in codebase
    Improves the handling of localization key references
    by utilizing grouping for regex matching, which optimizes
    performance and prevents overwhelming the regex engine.
    
    Refines regex patterns for better accuracy in identifying
    key references and updates type handling for remapped keys.
    
    Ensures seamless integration by adding necessary imports
    where needed.

--- lib/src/usecases/arb_related/resolve_l10n_keys_ref_in_codebase.dart
+++ lib/src/usecases/arb_related/resolve_l10n_keys_ref_in_codebase.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:enchanted_collection/enchanted_collection.dart';
 import 'package:gobabel/src/core/babel_failure_response.dart';
 import 'package:gobabel/src/entities/translation_payload_info.dart';
 import 'package:gobabel/src/flows_state/generate_flow_state.dart';
@@ -11,16 +12,16 @@ import 'package:gobabel_core/gobabel_core.dart';
 import 'package:result_dart/result_dart.dart';
 
 AsyncBabelResult<TranslationPayloadInfo>
-replaceAllL10nKeyReferencesInCodebaseForBabelFunctions({
+    replaceAllL10nKeyReferencesInCodebaseForBabelFunctions({
   required L10nProjectConfig projectConfig,
   required CodeBaseYamlInfo codeBaseYamlInfo,
-  required Map<TranslationKey, ProcessedKeyIntegrity> remapedArbKeys,
+  required Map<L10nKey, ProcessedKeyIntegrity> remapedArbKeys,
   required List<File> targetFiles,
   required TranslationPayloadInfo currentPayloadInfo,
   required String directoryPath,
 }) async {
-  final L10nProjectConfigWithData? projectConfigWithData = projectConfig
-      .mapOrNull(withData: (value) => value);
+  final L10nProjectConfigWithData? projectConfigWithData =
+      projectConfig.mapOrNull(withData: (value) => value);
   if (projectConfigWithData == null) {
     return currentPayloadInfo.toSuccess();
   }
@@ -28,13 +29,10 @@ replaceAllL10nKeyReferencesInCodebaseForBabelFunctions({
     ...currentPayloadInfo.keyToContextsPaths,
   };
   final String outputClass = projectConfigWithData.outputClass;
-  final String defaultPattern =
-      '$outputClass'
-      r'\s*\.of\(\s*(?:[a-zA-Z]|\r|\n|\t|\f|\v)+\s*,?\s*\)';
-  // Based on "final localizations = Localizations.of<S>(context, S);"
-  final String directDelegate =
-      '$outputClass'
-      r'\s*.of\(\s*(?:[a-zA-Z])+,\s*S\s*,?\s*\)';
+  final String defaultPattern = '$outputClass'
+      r'\s*\.of\(\s*(?:[a-zA-Z]|\r|\n|\t|\f|\v)+\s*,?\s*\)\s*!?';
+  final String directDelegate = '$outputClass'
+      r'\s*.of\(\s*(?:[a-zA-Z])+,\s*S\s*,?\s*\)\s*!?';
 
   for (final file in targetFiles) {
     try {
@@ -46,11 +44,43 @@ replaceAllL10nKeyReferencesInCodebaseForBabelFunctions({
         multiLine: true,
       );
       bool hasChanges = false;
-
-      fileContent.replaceAllMapped(regex, (match) {
+      fileContent = fileContent.replaceAllMapped(regex, (match) {
         hasChanges = true;
-        return outputClass;
+        return kBabelClass;
       });
+      // Let's group the entries so we won't overhelm the regex engine
+      final clusteredRemapedArbs = remapedArbKeys.entries.splitIntoGroups(30);
+      for (final group in clusteredRemapedArbs) {
+        String variableNamesIdentifiers = '';
+
+        for (final entry in group) {
+          final L10nKey originalKey = entry.key;
+          variableNamesIdentifiers = variableNamesIdentifiers.isEmpty
+              ? originalKey
+              : '$variableNamesIdentifiers|$originalKey';
+        }
+
+        final groupRegex = RegExp(
+          kBabelClass + r'\s*\.\s*' + '($variableNamesIdentifiers)',
+          caseSensitive: false,
+          multiLine: true,
+        );
+
+        fileContent = fileContent.replaceAllMapped(groupRegex, (match) {
+          final L10nKey? originalKey = match.group(1);
+          if (originalKey == null) {
+            return match.group(0) ?? '';
+          }
+
+          final ProcessedKeyIntegrity? newProcessedKey =
+              remapedArbKeys[originalKey];
+          if (newProcessedKey == null) {
+            return match.group(0) ?? '';
+          }
+
+          return '$kBabelClass.$newProcessedKey';
+        });
+      }
 
       if (hasChanges) {
         final fileContentWithImport = addImportIfNeededUsecase(
@@ -72,9 +102,8 @@ replaceAllL10nKeyReferencesInCodebaseForBabelFunctions({
 }
 
 AsyncBabelResult<
-  GenerateFlowReplacedAllL10nKeyReferencesInCodebaseForBabelFunctions
->
-generate_replaceAllL10nKeyReferencesInCodebaseForBabelFunctions(
+        GenerateFlowReplacedAllL10nKeyReferencesInCodebaseForBabelFunctions>
+    generate_replaceAllL10nKeyReferencesInCodebaseForBabelFunctions(
   GenerateFlowAppliedCodebaseGeneralDartFixes payload,
 ) async {
   final projectConfig = payload.projectArbData.mapOrNull(
